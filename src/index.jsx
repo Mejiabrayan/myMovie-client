6612f9b05d1d47dc4f2d9c637b98ee6d2a33f3eb
mejiabrayan <learnwithbrayan@gmail.com>
2022-09-01 09:53:01 -0700

    added new components

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle ./index.scss
 import './index.scss';
@@ -7,17 +8,11 @@ import './index.scss';
 // Main component (will eventually use all the others)
 class MyMovieApplication extends React.Component {
   render() {
-    return (
-      <div className='my-movie'>
-        <div>Good Morning</div>
-      </div>
-    );
+    return <MainView />;
   }
 }
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
-
-// Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyMovieApplication), container);
